ad12eaee9b1a4c8fe2f570b3d348b47f2b9fe73b
Andri <dripriyadi@gmail.com>
2025-04-29 07:41:45 +0700

    starting css

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,8 @@ export default function Index() {
    className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-light-300 font-bold">Welcome</Text>
+      <Link href={"/onboarding"}>Onboarding</Link>
+      <Link href={"/movie/Avengers"}>Avengers</Link>
     </View>
   );
 }
